388605b197d04b412771c5d976f219b53f354736
sujaya-nathashia <sujayanathashia@gmail.com>
2024-06-17 21:08:29 +0530

    first file

---
+++ sumOfTwoNumber.py
@@ -0,0 +1,11 @@
+# Get user input for the first number
+number1 = float(input("Enter the first number: "))
+
+# Get user input for the second number
+number2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+sum = number1 + number2
+
+# Print the result
+print("The sum of", number1, "and", number2, "is:", sum)
